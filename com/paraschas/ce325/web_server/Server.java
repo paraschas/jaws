7bf4da75a287aea324feab031e6bab4062a5b037
Dimitrios Paraschas <paraschas@gmail.com>
2015-05-03 01:03:40 +0300

    update version

--- com/paraschas/ce325/web_server/Server.java
+++ com/paraschas/ce325/web_server/Server.java
@@ -19,7 +19,7 @@ import com.paraschas.ce325.web_server.Settings;
  * A simple web server.
  *
  * @author   Dimitrios Paraschas <paraschas@gmail.com>
- * @version  0.0.1
+ * @version  0.0.2
  */
 public class Server {
     Settings settings;
